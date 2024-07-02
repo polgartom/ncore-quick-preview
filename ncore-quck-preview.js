a4ee1dc34270fc487795ddc45db212e71fac894b
polgar tamas <polgarey@gmail.com>
2024-07-02 10:52:17 +0200

    update

--- ncore-quck-preview.js
+++ ncore-quck-preview.js
@@ -1,33 +1,33 @@
 async function sssim() {
 	let boxes = [];
 	let selected = null;
-	
+
 	const find_parent = (h, clname) => {
 		while (h.parentElement) {
 			h = h.parentElement;
 			if (h.className.indexOf(clname) !== -1) return h;
 		}
-		
+
 		return null;
 	};
-	
+
 	const show_prev_images = (box, event) => {
 		// Prevent multiple preview instances at once
 		hide_all();
-		
+
 		if (box.PREVIEWS_FETCHED && box.container) {
 			event = event || window.event; // IE-ism
-						
+
 			const c = box.container;
 			c.style.display = 'block';
-			
+
 			const xy = c.getBoundingClientRect();
-			
+
 			//c.style.top  = `${event.clientY}px`;
 			c.style.left = `${box.getBoundingClientRect().x-15}px`;
 		}
 	};
-	
+
 	const hide_all = () => {
 		for (const b of boxes) {
 			if (b.container && b.container.style.display === "block") {
@@ -35,40 +35,40 @@ async function sssim() {
 			}
 		}
 	};
-	
+
 	const hide_prev_images = (box) => {
 		if (box.PREVIEWS_FETCHED && box.container) {
 			box.container.style.display = 'none';
 			return;
 		}
 	};
-	
+
 	let items = [...document.querySelectorAll('[onclick*="konyvjelzo("]')];
 	for (const it of items) {
 		let box = find_parent(it, 'box_torrent');
 		if (!box) continue; // probably we can break it too
-		
+
 		boxes.push(box);
 		box.onmouseenter = async (event) => {
 			selected = box;
-			
+
 			if (box.PREVIEWS_FETCHED) {
 				//show_prev_images(box, event);
 				return;
 			}
-			
+
 			box.PREVIEWS_FETCHED = true;
 			box.container = document.createElement('div');
 			box.container.style = "position:absolute;left:0;width:200px;min-height:300px;transform:translateX(-100%) translateY(-50%);background:#fff;padding:5px;border:2px solid #111;pointer-events:none;";
 			box.container.style.display = "none";
-			
+
 			{
 				const arrow = document.createElement('div');
 				arrow.textContent = '<';
 				arrow.style = 'position:absolute;left:100%;top:55%;transform: translateX(10px) translateY(-50%);color:#fff;font-weight:bold;';
 				box.container.appendChild(arrow);
 			}
-			
+
 			const s = it.getAttribute('onclick');
 			let r = '';
 			for (i = 0; i < s.length; i++) {
@@ -76,8 +76,8 @@ async function sssim() {
 					r += s[i];
 				}
 			}
-			
-			let resp = await fetch('ajax.php?action=torrent_drop&id='+r);
+
+			let resp = await fetch('ajax.php?action=torrent_drop&id=' + r);
 			resp = await resp.text();
 			let h = document.createElement('html');
 			if (h) {
@@ -97,26 +97,31 @@ async function sssim() {
 				}
 			}
 		};
-		
+
 		box.onmouseleave = () => {
 			selected = null;
 			hide_prev_images(box);
 		};
 
 	}
-	
-	window.addEventListener("keydown", (event) => {
-		if (event.key === "Shift" && selected) {
-			show_prev_images(selected, event);
-		}
-	});
-	
-	window.addEventListener("keyup", (event) => {
-		if (event.key == "Shift") {
+
+	let last = null;
+	window.addEventListener("mousemove", (event) => {
+		if (event.shiftKey) {
+			if (last !== selected) {
+				show_prev_images(selected, event);
+				last = selected;
+			}
+		} else if(last !== null) {
 			hide_all();
-			return;
+			last = null;
+			console.log("hide:mousemove")
 		}
 	});
-	
+
 }
+
 sssim();
+
+
+
